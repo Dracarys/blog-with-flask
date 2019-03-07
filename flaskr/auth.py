4b1db896ceb3a98a5936bd320b1184e2514bbd3a
Dracarys <cavalry001@hotmail.com>
2019-03-07 16:59:40 +0800

    fix some syntax error

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -11,8 +11,8 @@ from flaskr.db import get_db
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
-@bp.route('/register', method=('GET', 'POST'))
-def refister():
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
 	if request.method == 'POST':
 		username = request.form['username']
 		password = request.form['password']
@@ -23,25 +23,20 @@ def refister():
 			error = 'username is required.'
 		elif not password:
 			error = 'Password is required.'
-		elif db.execute(
-			'SELECT id FROM user WHERE username = ?', (username,)
-		).fetchone() is not None:
+		elif db.execute('SELECT id FROM user WHERE username = ?', (username,)).fetchone() is not None:
 			error = 'User {} is already registered.'.format(username)
 
 		if error is None:
-			db.execute(
-				'INSERT INTO user (username, password) VALUES (?, ?)',
-				(username, generate_password_hash(password))
-			)
+			db.execute('INSERT INTO user (username, password) VALUES (?, ?)', (username, generate_password_hash(password)))
 			db.commit()
 			return redirect(url_for('auth.login'))
 
-	    flash(error)
+		flash(error)
 
 	return render_template('auth/register.html')
 
 
-@bp.route('/login', method=('GET', 'POST'))
+@bp.route('/login', methods=('GET', 'POST'))
 def login():
 	if request.method == 'POST':
 		username = request.form['username']
@@ -63,7 +58,7 @@ def login():
 			session['user_id'] = user['id']
 			return redirect(url_for('index'))
 
-	    flash(error)
+		flash(error)
 
 	return render_template('auth/login.html')
 
