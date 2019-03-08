a54060b6dcd2c4d4ccc4076f47d0da38bbbcf9a7
Dracarys <cavalry001@hotmail.com>
2019-03-08 10:43:10 +0800

    fix some bugs

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -13,9 +13,9 @@ bp = Blueprint('blog', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, bogy, created, author_id, username'
-        'FROM post p JOIN user u ON p.author_id = u.id'
-        'ORDER BY created DESC'
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
 
@@ -47,7 +47,7 @@ def create():
 
 
 def get_post(id, check_author=True):
-    post = get_db.execute(
+    post = get_db().execute(
         'SELECT p.id, title, body, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
@@ -68,7 +68,7 @@ def get_post(id, check_author=True):
 def update(id):
     post = get_post(id)
 
-    if request.methods == 'POST':
+    if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
         error = None
